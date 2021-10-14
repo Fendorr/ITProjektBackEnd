d3d939cbfbb92c315947ae007d0f7def3fe0d33a
Fendorr <JeaSchneider@web.de>
2021-10-14 16:50:40 +0200

    localTestCommit#2 für Authentifizierung

--- src/main/java/com/restful/api/h2/example/SecurityConfig.java
+++ src/main/java/com/restful/api/h2/example/SecurityConfig.java
@@ -34,7 +34,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception{
         http.authorizeRequests()
-                .antMatchers("/**").permitAll();
+                .antMatchers("/**").permitAll()
+                .and().formLogin();
     }
 
     @Bean
