c32e31667b2b16151d68b6f22b31ac1e35abbd98
Rakanishuu <domiswelt2@web.de>
2021-11-23 21:11:30 +0100

    usertags

--- src/main/java/com/restful/api/h2/example/Boundry/model/UserDTO.java
+++ src/main/java/com/restful/api/h2/example/Boundry/model/UserDTO.java
@@ -22,6 +22,7 @@ public class UserDTO {
     Long[] likedProjects;
     Long[] projectInvites;
     Long[] sentApplications;
+    String[] tags;      //Keywords
 
     //region Getter/Setter
     public String getFirstName() {
@@ -110,6 +111,14 @@ public class UserDTO {
 
     public void setSentApplications(Long[] sentApplications) {
         this.sentApplications = sentApplications;
+    }
+
+    public String[] getTags() {
+        return tags;
+    }
+
+    public void setTags(String[] tags) {
+        this.tags = tags;
     }
 
     //endregion
